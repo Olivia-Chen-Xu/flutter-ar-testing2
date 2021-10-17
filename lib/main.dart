8167b672931da6d252a9410f94d22bc03f4e3dc9
Olivia-Chen-Xu <oliviachenxu@gmail.com>
2021-10-17 09:10:00 -0400

    added padding

--- lib/main.dart
+++ lib/main.dart
@@ -112,44 +112,50 @@ class _MyHomePageState extends State<MyHomePage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Card(
-                child: Column(children: [
-                  Text('Nivea Toner',
-                      style:
-                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
-                  SizedBox(
-                    height: 30,
-                    width: 80,
-                    child: FloatingActionButton.extended(
-                      onPressed: () {
-                        // Add onPressed function
-                      },
-                      label: const Text('Add to Cart',
-                          style: TextStyle(fontSize: 10)),
-                      icon: const Icon(Icons.thumb_up),
-                      backgroundColor: Colors.pink,
-                    ),
-                  )
-                ]),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(children: [
+                    Text('Nivea Toner',
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold)),
+                    SizedBox(
+                      height: 25,
+                      width: 150,
+                      child: FloatingActionButton.extended(
+                        onPressed: () {
+                          // Add onPressed function
+                        },
+                        label: const Text('Add to Cart',
+                            style: TextStyle(fontSize: 10)),
+                        icon: const Icon(Icons.thumb_up),
+                        backgroundColor: Colors.pink,
+                      ),
+                    )
+                  ]),
+                ),
               ),
               Card(
-                child: Column(children: [
-                  Text('Glysomed Hand Cream',
-                      style:
-                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold)),
-                  SizedBox(
-                    height: 30,
-                    width: 80,
-                    child: FloatingActionButton.extended(
-                      onPressed: () {
-                        // Add onPressed function
-                      },
-                      label: const Text('Add to Cart',
-                          style: TextStyle(fontSize: 10)),
-                      icon: const Icon(Icons.thumb_up),
-                      backgroundColor: Colors.pink,
-                    ),
-                  )
-                ]),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(children: [
+                    Text('Glysomed Hand Cream',
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold)),
+                    SizedBox(
+                      height: 25,
+                      width: 150,
+                      child: FloatingActionButton.extended(
+                        onPressed: () {
+                          // Add onPressed function
+                        },
+                        label: const Text('Add to Cart',
+                            style: TextStyle(fontSize: 10)),
+                        icon: const Icon(Icons.thumb_up),
+                        backgroundColor: Colors.pink,
+                      ),
+                    )
+                  ]),
+                ),
               )
             ],
           ))
